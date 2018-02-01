233c2beb63295ee563f645f5d286d74bf53a3820
Dongqing Xiao (MSR Student-FA Talent) <v-doxiao@microsoft.com>
2018-02-01 15:31:24 +0800

    update the SGD_ES with simple GA idea

--- SGD_ES.py
+++ SGD_ES.py
@@ -4,6 +4,18 @@ import random
 import copy 
 import numpy as np 
 
+def compute_ranks(x):
+  """
+  Returns ranks in [0, len(x))
+  Note: This is different from scipy.stats.rankdata, which returns ranks in [1, len(x)].
+  (https://github.com/openai/evolution-strategies-starter/blob/master/es_distributed/es.py)
+  """
+  assert x.ndim == 1
+  ranks = np.empty(len(x), dtype=int)
+  ranks[x.argsort()] = np.arange(len(x))
+  return ranks
+
+
 class SGDS_ES():
     
     def __init__(self, net,lr=0.001,momentum=0.99):
@@ -27,53 +39,68 @@ class SGDS_ES():
     def step(self,net,inputs,labels,criterion,flag):
         loss_min=None
         lossnum=None
-        choiceList=random.sample(self.num_list,8)
-        reward = np.zeros(8)
-        # let us assume this es
-
-        for num in choiceList:
+       
+        reward =np.zeros(8)
+        # keep elite and remove 
+        num_list=self.num_list
+        
+        for num in num_list:
             for name,parameters in net.named_parameters():
-                #self.V[num][name]=self.momentum*self.V[num][name]+parameters.grad.data # update history 
-                parameters.data=parameters.data-(self.momentum*self.V[num][name]+parameters.grad.data)*self.lr
+                
+                self.V[num][name]=self.momentum*self.V[num][name]+parameters.grad.data # update history 
+                parameters.data=parameters.data-self.V[num][name]*self.lr
             
             outputs=net(inputs)
             loss=criterion(outputs,labels)
-            reward[num]=-1*loss 
+            reward[num] = -1*(loss.data[0]) # larger reward means less loss 
+            
             
             if (loss_min is None) or (loss.data[0] < loss_min).all():
                 loss_min=loss.data
                 lossnum=num
             for name,parameters in net.named_parameters():
-                parameters.data=parameters.data+(self.momentum*self.V[num][name]+parameters.grad.data)*self.lr
+                parameters.data=parameters.data+self.V[num][name]*self.lr
         
-        self.popsize=len(choiceList) 
-        # def submate()
+        
+        #print(reward)  
         def mate(a,b):
             vsample=copy.copy(self.V[a])
             for name,parameters in net.named_parameters():
+                
                 c=np.random.rand()>0.5 
                 if c:
                     vsample[name]=self.V[b][name]
             return vsample
-        #
-        #keep the elite samples and random-cross over to generate the other variants 
-        #add simple random noise 
-        choices=np.random.choice(self.num_list,2*self.popsize,list(reward))     
-        solutions=[mate(choices[2*i],choices[2*i+1]) for i in range(self.popsize)]
-         
-        # for i in range(self.popsize):
-        #     if num!=lossnum:
-        #         self.V[num]=solutions[i]
-        i=0
-        for num in choiceList:
-            if num!=lossnum:
-                self.V[num]=solutions[i]
-            else:
-                self.V[num]=
-            i+=1
-
+        
+        def mute(sample):
+            for name,parameters in net.named_parameters():
+                data=sample[name].cpu().numpy()
+                print(data.shape)
+                ep =np.random.randn(data.shape)*0.001
+                data +=ep 
+                sample[name]=torch.from_numpy(data).float()
+                
        
-        #moving around the best_mu 
-        #cross-over the history 
         for name,parameters in net.named_parameters():
             parameters.data=parameters.data-self.V[lossnum][name]*self.lr
+        elite_popsize=4
+        reward=np.array(reward) 
+        
+        print(reward.std())
+        elite_size=idx = np.argsort(reward)[::-1][0:elite_popsize]  
+        elite_params=[]
+        for item in elite_size:
+            elite_params.append(self.V[item])  
+        elite_reward=reward[elite_size]
+        
+        elite_range=[item for item in range(elite_popsize)]
+        popsize=8-elite_popsize
+        
+        choices=np.random.choice(elite_range,2*popsize,list(elite_reward))
+        solutions=[mate(choices[2*i],choices[2*i+1]) for i in range(popsize)] #cross-over 
+        _=[mute(s) for s in solutions]
+        
+        elite_params+=solutions
+        
+        for idx, num in enumerate(num_list):
+            self.V[num]=elite_params[idx]
