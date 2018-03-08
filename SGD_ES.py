00fea150d4bf467057da0bb585bc0aeba9bea593
dongqing <xiaoguohaoke@gmail.com>
2018-03-08 17:14:38 +0800

    adding SGD ES methods

--- SGD_ES.py
+++ SGD_ES.py
@@ -25,9 +25,9 @@ def cal_nparams(model):
 
 
 class SGDmomentum():
-    def __init__(self,net,lr=0.001,momentum=0.99):
+    def __init__(self,net,momentum,lr=0.001):
         self.lr=lr 
-        self.momentum=0.99 
+        self.momentum=momentum 
         self.momentum_buffer={} 
         for n,p in net.named_parameters():
             self.momentum_buffer[n]=t.zeros_like(p.data)
@@ -49,8 +49,97 @@ class SGDmomentum():
 
             p.data.add_(-1*lr*self.momentum_buffer[n])
 
-        return 1,0 
+        return 1,0,self.momentum
 
+    
+class SGD_OES_NN():
+    # momentum: uniform distribution 
+    # 
+    def __init__(self,net,lr=0.001, momentum=0.99):
+        self.lr = lr 
+        self.v = {}
+        self.popsize = 8
+        self.sigma = 0.01 
+        self.hlr = 0.1   # hyper-learning rate 
+        self.momentum = momentum 
+
+        for name, p in net.named_parameters():
+            self.v[name] = t.zeros_like(p.data)
+
+
+#         self.mu = np.array([self.momentum,1])  # [2,1] 
+        self.base  = self.alpha_reTrans(momentum)
+        self.mu = np.array([self.base,1])
+        self.num_params =2 
+
+
+    def ask(self):
+
+        self.epsilon = np.zeros([self.popsize,2])
+        self.epsilon[:,0] = np.random.normal(0,self.sigma,self.popsize) # uniformal distribution 
+        self.epsilon[:,1] = np.random.normal(0,self.sigma,self.popsize) #normal distribution 
+
+
+    def alpha_trans(self,i):
+        return 1-np.power(0.1,i)
+    
+
+    def alpha_reTrans(self,val):
+        return -1*np.log10(1-val)
+        
+    def step(self, net, data, target, base):
+        lossnum= -1 
+        loss_min = base  
+        lr = self.lr 
+
+      
+        self.ask()
+        reward = np.zeros(self.popsize) 
+        
+        for i in range(self.popsize):
+
+            for name, p in net.named_parameters():
+                d_p =  p.grad.data
+                temp =  self.v[name]*(self.alpha_trans(self.mu[0]+self.epsilon[i][0]))
+                temp += d_p * (self.mu[1]+self.epsilon[i][1]) 
+
+                p.data.add_(-1*lr*temp)
+
+            output=net(data)
+            loss = F.nll_loss(output, target)  
+
+            reward[i] = -loss.data[0] 
+            # find the best index 
+            if (loss.data < loss_min).all:
+                loss_min=loss.data   
+                lossnum=i
+            # recover 
+            for name, p in net.named_parameters():
+                d_p =  p.grad.data
+                temp =  self.v[name]*(self.alpha_trans(self.mu[0]+self.epsilon[i][0]))
+                temp += d_p * (self.mu[1]+self.epsilon[i][1]) 
+
+                p.data.add_(lr*temp)
+
+        #reward = compute_centered_ranks(reward)   
+
+        normalized_reward = (reward - np.mean(reward)) / np.std(reward)
+        #update the expectation of 1 
+        self.mu += self.hlr/(self.popsize*self.sigma)*np.dot(self.epsilon.T, normalized_reward) # update the beta 
+        #what happens 
+        
+        for name, p in net.named_parameters():
+            d_p = p.grad.data
+            self.v[name].mul_(self.alpha_trans(self.mu[0])).add_(d_p*self.mu[1])
+            p.data.add_(-1*lr*self.v[name])
+
+        #reset the beta to the original one 
+        self.mu[1]= 1 
+        c_momentum= self.mu[0]
+        self.mu[0]=self.base 
+        
+
+        return loss_min[0], np.std(reward), self.alpha_trans(c_momentum)
 
 class SGD_OES():
     
@@ -59,26 +148,198 @@ class SGD_OES():
         self.v = {} # dict for recording 
         self.var = {}
         self.popsize = 8
-        self.sigma = 0.001 
-        # setting up the \mu 
+        self.sigma = 0.01 
+        self.hlr = 0.1   # hyper-learning rate 
+       
         for name, p in net.named_parameters():
             self.v[name]=t.zeros_like(p.data)
-            self.var[name]= [t.zeros_like(p.data).normal_()*self.sigma for i in range(self.popsize)]
+            self.v[name+"g"]=1 
+            self.var[name]=np.random.normal(0,self.sigma,self.popsize) 
+           
         self.v['momentum']= momentum 
-        self.var['momentum']=[2*self.sigma*(random.random()-0.5)+momentum for i in range(self.popsize)]          
-    
+        self.momentum = momentum
+        self.var['momentum']=np.random.normal(0,self.sigma,self.popsize)         
+        
+        
     def step(self, net, data, target, base):
-        best_index= -1 
+        lossnum= -1 
+        loss_min = base  
         lr = self.lr 
+
+        reward = np.zeros(self.popsize) 
+        
+#         self.v['momentum'] = self.momentum
+#         for name,p in net.named_parameters():
+#             self.var[name+"g"]=1 
+  
+        # using this object 
         for i in range(self.popsize):
-            for n, p in net.named_parameters():
-                    d_p =  p.grad.data
-                    temp = self.v[n]*self.var['momentum']+d_p
-                    p.data.add_(-1*lr*temp)
-             
+
+            for name, p in net.named_parameters():
+                d_p =  p.grad.data
+                temp =  self.v[name]*(self.v['momentum']+self.var['momentum'][i])
+                
+                temp =  temp + d_p*(self.v[name+"g"]+self.var[name][i])
+                p.data.add_(-1*lr*temp)
+
+            output=net(data)
+            loss = F.nll_loss(output, target)  
+
+            reward[i] = -loss.data[0] 
+            # find the best index 
+            if (loss.data < loss_min).all:
+                loss_min=loss.data   
+                lossnum=i
+            # recover 
+            for name, p in net.named_parameters():
+                d_p  = p.grad.data
+                temp =  self.v[name]*(self.v['momentum']+self.var['momentum'][i])
+                temp =  temp + d_p*(self.v[name+"g"]+self.var[name][i])
+                p.data.add_(lr*temp)
+
+#         reward = compute_centered_ranks(reward)   
+
+        normalized_reward = (reward - np.mean(reward)) / np.std(reward)
+        #update the expectation of 1, momentum 
+        self.v['momentum'] += self.hlr/(self.popsize*self.sigma)*np.dot(self.var['momentum'].T, normalized_reward)
+        for name, p in net.named_parameters():
+            d_p = p.grad.data
+            self.v[name+"g"] += self.hlr/(self.popsize*self.sigma)*np.dot(self.var[name].T, normalized_reward)
             
+            self.v['momentum'] = min(0.999, self.v['momentum'])
+            self.v['momentum'] = max(0.9, self.v['momentum'])
+            
+            self.v[name].mul_(self.v['momentum']).add_(d_p*(self.v[name+"g"]))
+            p.data.add_(-1*lr*self.v[name]) 
+            self.v[name+"g"] = 1 
+        
+        return loss_min[0], np.std(reward), self.v['momentum']
+             
+
+class SGD_PEPG():
+    # let us consider single g 
+    def __init__(self,net,lr=0.001, momentum=0.99):
+        self.lr = lr 
+        self.v = {} # dict for recording 
+
+        self.popsize = 8
+        self.sigma_init = 0.01 
+        self.hlr = 0.1    # hyper-learning rate 
+        self.hlr_limit = 0.01 
+        self.sigma_alpha = 0.2 
+
+        for name, p in net.named_parameters():
+            self.v[name]=t.zeros_like(p.data)
+        
+
+        self.batch_size = int((self.popsize)/2)  
+        self.num_params = 2 
+        self.momentum = momentum
+        self.mu = np.array([momentum,1]) # 
+        self.sigma = np.ones(self.num_params)*self.sigma_init 
+
+    def ask(self):
+        # anthithetic sampling 
+
+        self.epsilon = np.random.randn(self.batch_size,self.num_params)*self.sigma.reshape(1,self.num_params)
+        self.epsilon_full = np.concatenate([self.epsilon, -self.epsilon])
+
+    def step(self, net, data, target, base):
+        loss_min=base 
+        lossnum= -1 
+        reward = np.zeros(self.popsize)
+        lr = self.lr 
+        self.ask()
+        #print(self.epsilon)
         
         
+        for name, p in net.named_parameters():
+            d_p = p.grad.data
+            temp = self.v[name]*(self.mu[0])
+            temp = temp + d_p 
+            p.data.add_(-1*lr*temp)
+        
+        output = net(data)
+        loss = F.nll_loss(output, target)
+        b = -loss.data[0]
+        
+        for name, p in net.named_parameters():
+            d_p = p.grad.data
+            temp = self.v[name]*(self.mu[0])
+            temp = temp + d_p 
+            p.data.add_(lr*temp)
+        
+        
+        for i in range(self.popsize):
+            #move one noisy step 
+            for name, p in net.named_parameters():
+                d_p =  p.grad.data
+                temp =  self.v[name]*(self.mu[0]+self.epsilon_full[i,0]) 
+                temp =  temp + d_p*(self.mu[1]+self.epsilon_full[i,1])
+                p.data.add_(-1*lr*temp)
+
+            output=net(data)
+            loss = F.nll_loss(output, target)  
+
+            reward[i] = -loss.data[0] 
+            # find the best index 
+            if (loss.data < loss_min).all:
+                loss_min=loss.data   
+                lossnum=i
+            # recover the model 
+            for name, p in net.named_parameters():
+                d_p =  p.grad.data
+                temp =  self.v[name]*(self.mu[0]+self.epsilon_full[i,0]) 
+                temp =  temp + d_p*(self.mu[1]+self.epsilon_full[i,1])
+                p.data.add_(lr*temp)
+                
+#         normalized_reward = (reward - np.mean(reward)) / np.std(reward)
+        # update the sigma and mu 
+#         b= -1*base
+        stdev_reward = reward.std() 
+        epsilon = self.epsilon
+        sigma = self.sigma
+        S = ((epsilon * epsilon - (sigma * sigma).reshape(1, self.num_params)) / sigma.reshape(1, self.num_params))
+        reward_avg = (reward[:self.batch_size] + reward[self.batch_size:]) / 2.0
+        rS = reward_avg - b
+        delta_sigma = (np.dot(rS, S)) / (2 * self.batch_size * stdev_reward)
+
+
+        rT = (reward[:self.batch_size] - reward[self.batch_size:])
+
+        # update the mu 
+        change_mu = self.hlr * np.dot(rT, epsilon)
+        self.mu += change_mu
+        self.mu[0] = min(0.999, self.mu[0])
+        self.mu[0] = max(0.9, self.mu[0])
+
+        # update the sigma 
+        change_sigma = self.sigma_alpha * delta_sigma
+
+#         change_sigma = np.minimum(change_sigma, self.sigma)
+#         change_sigma = np.maximum(change_sigma, - 0.5 * self.sigma)
+
+        self.sigma += change_sigma
+
+#         if(self.hlr> self.hlr_limit):
+#             self.hlr *=0.999
+
+
+
+        # one real step forward  
+        for name, p in net.named_parameters():
+            d_p =  p.grad.data
+            self.v[name].mul_(self.mu[0]).add_(d_p*self.mu[1])
+
+            p.data.add_(-1*lr*self.v[name])
+        #reset-the g multilper 
+        self.mu[1]=1
+        c_momentum= self.mu[0]
+#         self.mu[0]=self.momentum
+        
+        return loss_min[0], np.std(reward), c_momentum
+
+    
         
         
     
--- sgd_main.py
+++ sgd_main.py
@@ -20,6 +20,9 @@ from fileIO import *
 import os 
 import sys 
 from SGD_ES import * 
+from model.vgg import * 
+from model.NetworkModel import * 
+
 
 def mnistFeed():
 	global train_loader
@@ -40,21 +43,49 @@ def mnistFeed():
 
 	return "MNIST"
 
-#done 
+def cifar10Feed():
+	global train_loader
+	global valid_loader
+	global test_loader
+	
+	transform_train = transforms.Compose([
+	transforms.RandomCrop(32, padding=4),
+	transforms.RandomHorizontalFlip(),
+	transforms.ToTensor(),
+	transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+	])
+
+	transform_test = transforms.Compose([
+	transforms.ToTensor(),
+	transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+	])
+
+	trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+	train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+
+	valid_loader = train_loader
+
+
+	testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+	test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=2)
+
+	return "CIFAR10"
+
 
 
 def train(epoch,printTrain=True):
 	model.train()
 	for batch_idx, (data, target) in enumerate(train_loader):
-	
+		if args.cuda:
+			data, target = data.cuda(), target.cuda()
 		data, target= Variable(data), Variable(target)
 		output = model(data)
 		model.zero_grad()
 		loss = F.nll_loss(output, target) 
 		loss.backward()
-		best_reward,reward_std=optimizer.step(model,data,target,loss.data[0])
+		best_reward,reward_std,cm=optimizer.step(model,data,target,loss.data[0])
 		if batch_idx % 100 ==0:
-			print('{}\t{}\t{}'.format(loss.data[0],-1*best_reward,reward_std)) 
+			print('{}\t{}\t{}\t{}'.format(loss.data[0],-1*best_reward,reward_std,cm)) 
 
 		# if batch_idx % 1000 == 0 and printTrain:
 		# 	print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
@@ -78,8 +109,8 @@ if __name__=="__main__":
 
 	parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 	parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-	parser.add_argument('--model',default="VGG16",help='DNN model')
-	parser.add_argument('--optimizer',default='PEPG',help='SGD or ES methods used')
+	parser.add_argument('--model',default="Net",help='DNN model')
+	parser.add_argument('--optimizer',default='SGDM',help='SGD or ES methods used')
 	parser.add_argument('--popsize',default=0, type=int, help='ES popsize')
 	parser.add_argument('--opt',default=1, type=int, help='ES sigma option')
 	parser.add_argument('--diversity_base',default=0.0, type=float, help='diversity up bounded')
@@ -87,32 +118,46 @@ if __name__=="__main__":
 	parser.add_argument('--epochs',default=100, type=int, help='the number of iteration')
 	parser.add_argument('--batch_size',default=100,type=int,help='batch size')
 	parser.add_argument('--momentum',default=0.99,type=float,help='momentum')
-	# parser.add_argument()
+    
 	
 	args = parser.parse_args()
-	mnistFeed()
+	datasetName=cifar10Feed()
 
 
-	model = MLPNet()
-	# NPARAMS,model_shapes=cal_nparams(model)
+# 	model = MLPNet()
+	if args.model=="Net":
+		model = Net()
+	else:
+		model = VGG(args.model)
+	if args.cuda:
+		torch.cuda.manual_seed(0)
+		model.cuda()
 	momentum =args.momentum
-	#optimizer = SGDS_ES(model,momentum=momentum)
-	# optimizer= SGDmomentum(model,momentum=momentum)
-	optimizer = SGD_GA(model,momentum=momentum)
+	if args.optimizer=="SGDM":
+		optimizer= SGDmomentum(model,momentum)
+	if args.optimizer=="SGDM_OES":
+		optimizer = SGD_OES(model,momentum=momentum)
+	if args.optimizer=="SGDM_PEPG":
+		optimizer = SGD_PEPG(model,momentum=momentum)
+	if args.optimizer=="SGDM_OES_NN":
+		optimizer = SGD_OES_NN(model,momentum=momentum)
+        
+
+# 	optimizer = SGD_GA(model,momentum=momentum)
 
 
 	#
 
 	training_log=[]
-	for epoch in range(1,100):
+	for epoch in range(1,args.epochs):
 		train(epoch,printTrain=True)
-		test_acc,test_loss=evaluate(model, test_loader, print_mode=False)
-		valid_acc,valid_loss=evaluate(model, valid_loader,print_mode=False)
+		test_acc,test_loss=evaluate(model, test_loader, print_mode=False,cuda=args.cuda)
+		valid_acc,valid_loss=evaluate(model, valid_loader,print_mode=False,cuda=args.cuda)
 		training_log.append([valid_acc,valid_loss,test_acc,test_loss])
-		print("{}\t{}\t{}".format(epoch,valid_loss,test_loss))
+		print("{}\t{}\t{}".format(epoch,test_loss,test_acc))
 
-	fname = "{}-{}".format("SGDM-oes",momentum)
-	folder ="{}/{}/".format("MNIST","MLP") 
+	fname = "{}-R-{}".format(args.optimizer,momentum)
+	folder ="{}/{}/".format(datasetName,model.name()) 
 	pickle_write(np.array(training_log),folder+fname,"-NN")
 
 	
